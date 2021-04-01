817e3ad007fe145ffe84ae154cc2326c3e29ea4a
vineet23 <53836576+vineet23@users.noreply.github.com>
2021-04-01 22:40:27 +0530

    heroku postgre
    heroku postgre and static

--- smallcase/settings.py
+++ smallcase/settings.py
@@ -75,14 +75,28 @@ WSGI_APPLICATION = 'smallcase.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "dcf0d1b83e9iuk",
+            "USER": "aesdfrtyqokmeq",
+            "PASSWORD": "25976b57d4f9c76677b01957f83060ac7e6d174ea5a0be1beb2e53faa9d14add",
+            "HOST": "ec2-18-214-208-89.compute-1.amazonaws.com",
+            "PORT": "5432",
+        }
 }
 
+"""DATABASES = {
+    "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "SmallCase",
+            "USER": "postgres",
+            "PASSWORD": "5143",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
